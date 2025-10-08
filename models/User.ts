1053b097b137059441815a4472473171193f8698
Sarthak Bhatnagar <sarthakbhatnagar103@gmail.com>
2025-10-08 16:40:04 +0530

    setting up models

--- models/User.ts
+++ models/User.ts
@@ -1,31 +1,34 @@
-import mongoose, {Schema,model,models} from "mongoose";
+import mongoose, { Schema, model, models } from "mongoose";
 import bcrypt from "bcryptjs";
 
 export interface IUser {
-    email:string;
-    password:string;
-    username:string;
-    _id?:mongoose.Types.ObjectId;
-    createdAt?:Date;
-    updatedAt?:Date;
+    email: string;
+    password: string;
+    username: string;
+    _id?: mongoose.Types.ObjectId;
+    createdAt?: Date;
+    updatedAt?: Date;
 }
 
 
 const userSchema = new Schema<IUser>(
     {
-        email:{type:String ,required:true,unique:true},
-        username:{type:String ,required:true,unique:true},
-        password:{type:String ,required:true}
+        email: { type: String, required: true, unique: true },
+        username: { type: String, required: true, unique: true },
+        password: { type: String, required: true }
     },
     {
-        timestamps:true
+        timestamps: true
     }
 );
 
-userSchema.pre('save', async function(next){
-    if(this.isModified("password")){
-        this.password = await bcrypt.hash(this.password,10)
+userSchema.pre('save', async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10)
     }
     next()
 })
 
+const User = models?.User || model<IUser>("User", userSchema)
+
+export default User
---
+++ models/Video.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+
+export const video_dimensions = {
+    width: 1080,
+    height: 1920
+} as const
+
+export interface IVideo {
+    _id?: mongoose.Types.ObjectId;
+    title: string;
+    description: string;
+    videoURL: string;
+    thumbnailURL: string;
+    constrols?: boolean;
+    transformation?: {
+        height: number;
+        width: number;
+        quality: number;
+    }
+};
+
+const videoSchema = new Schema<IVideo>(
+    {
+        title: { type: String, required: true },
+        description: { type: String, required: true },
+        videoURL: { type: String, required: true },
+        thumbnailURL: { type: String, required: true },
+        constrols: { type: String, required: true },
+        transformation: {
+            height: { type: Number, default: video_dimensions.height },
+            width: { type: Number, default: video_dimensions.width },
+            quality: { type: Number, min: 1, max: 100 },
+        },
+    },
+    {
+        timestamps: true
+    }
+)
+const Video = models?.Video || model<IVideo>("Video", videoSchema)
+
+export default Video
