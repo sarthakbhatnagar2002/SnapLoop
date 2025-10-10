5f0452e4e1c843baba6d9df03a7dd794b7cd3bd9
Sarthak Bhatnagar <sarthakbhatnagar103@gmail.com>
2025-10-11 00:13:35 +0530

    Adding middlewares and auth routes

---
+++ app/components/FileUpload.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import {
+    ImageKitAbortError,
+    ImageKitInvalidRequestError,
+    ImageKitServerError,
+    ImageKitUploadNetworkError,
+    upload,
+} from "@imagekit/next";
+import { useRef, useState } from "react";
+
+interface FileUploadProps {
+    onSuccess: (res: any) => void
+    onProgress?: (progress: number) => void
+    fileType?: "image" | "video"
+}
+
+const FileUpload = ({
+    onSuccess,
+    onProgress,
+    fileType
+}: FileUploadProps) => {
+
+
+    const [uploading, setUploading] = useState(false)
+    const [error, setError] = useState<string | null>(null)
+
+    const validateFile = (file: File) => {
+        if (fileType === "video") {
+            if (!file.type.startsWith("video/")) {
+                setError("Please upload a valid File")
+            }
+        }
+        if (file.size > 100 * 1024 * 1024) {
+            setError("File size must not exceed 100 MB")
+        }
+        return true
+    }
+
+    const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0]
+        if (!file || !validateFile(file)) return
+
+        setUploading(true)
+        setError(null)
+
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+
+    return (
+        <>
+            <input
+                type="file"
+                accept={fileType === "video" ? "video/*" : "image/*"}
+                onChange={handleFileChange}
+            />
+            {uploading && (
+                <span>Loading...</span>
+            )}
+        </>
+    );
+};
+
+export default FileUpload;
---
+++ app/components/Provider.tsx
@@ -0,0 +1,12 @@
+import { ImageKitProvider } from "@imagekit/next";
+import { SessionProvider } from "next-auth/react";
+
+const urlEndpt = process.env.NEXT_PUBLIC_URL_ENDPOINT
+
+export default function({children}:{children:React.ReactNode}){
+    return (
+        <SessionProvider refetchInterval={5*60}>
+            <ImageKitProvider>{children}</ImageKitProvider>
+        </SessionProvider>
+    )
+}
