5a8e217adecc396f52e06f1f2ba1ade6cc42966e
Sarthak Bhatnagar <sarthakbhatnagar103@gmail.com>
2025-11-04 00:10:19 +0530

    fixing params

--- app/api/profile/[username]/route.ts
+++ app/api/profile/[username]/route.ts
@@ -5,12 +5,12 @@ import Video from "@/models/Video";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { username: string } }
+  { params }: { params: Promise<{ username: string }> }
 ) {
   try {
     await connectToDb();
 
-    const { username } = params;
+    const { username } = await params;
 
     const user = await User.findOne({
       $or: [
